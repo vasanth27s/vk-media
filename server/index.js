e452641f1ff245169bbb120e5937d067d787b93c
vasanth <vasanth2003s@gmail.com>
2024-07-24 09:47:48 +0530

    vk-media

--- server/index.js
+++ server/index.js
@@ -31,7 +31,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://vk-media.vercel.app',
+  methods: 'GET, POST, PUT, DELETE',
+  credentials: true
+}));
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 app.use(express.static(path.join(__dirname, '/client/build')));
 
